20a6f4d92cf638e4a52f8204b9a8bfd1e6b0ff62
Wei Mei <56918345+nickcafferry@users.noreply.github.com>
2020-09-26 11:41:59 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -39,9 +39,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'remain']
 pygments_style = 'default'
 
 html_static_path = ['assets']
-html_theme = 'sphinx_rtd_theme'
-html_favicon = 'GCC.png'
-html_logo = 'GCC.png'
+html_theme = 'haiku'
 html_theme_options = {
     'logo_only': False,
     'style_nav_header_background': '#343131',
